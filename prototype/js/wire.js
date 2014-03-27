7d79b06da481c5045ccdd1d1164dd42786b81c98
curran <curran.kelleher@gmail.com>
2014-03-27 17:20:50 -0400

    Refactored wire module

--- prototype/js/wire.js
+++ prototype/js/wire.js
@@ -7,20 +7,26 @@
  *
  * See also https://github.com/curran/backboneComputedProperties
  *
- * Curran Kelleher 3/26/2014
+ * Example usage:
+ *
+ * ```
+ * wire(['firstName', 'lastName'], function (first, last) {
+ *   model.set('fullName', first + last);
+ * }, model);
+ * ```
+ *
+ * Curran Kelleher 3/27/2014
  */
 define([], function () {
   return function wire(dependencies, fn, model){
     var callFn = _.debounce(function(){
-      var args = dependencies.map(function (property){
+      fn.apply(null, dependencies.map(function (property){
         return model.get(property);
-      });
-      fn.apply(null, args);
+      }));
     });
-    // TODO consider using 'change:a change:b'
+    callFn();
     dependencies.forEach(function(property){
       model.on('change:' + property, callFn);
     });
-    callFn();
   }
 });
