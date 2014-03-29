6fb8393e59273fb6c21df289d90835a6cfc1dd00
curran <curran.kelleher@gmail.com>
2014-03-28 23:52:38 -0400

    Implemented the link module, for configuring links between visualization in dashboards with multiple linked views.

--- prototype/src/links.js
+++ prototype/src/links.js
@@ -2,10 +2,36 @@
 // supporting multiple linked views in a visualization dashboard.
 //
 // Curran Kelleher 3/26/2014
-define([], function () {
+define(['wire'], function (wire) {
 
   // The constructor function.
-  return function () {
-    return new Backbone.Model();
+  return function (dashboard) {
+    var model = new Backbone.Model({
+      bindings: []
+    });
+
+    wire(['bindings'], function (bindings) {
+      bindings.forEach(function (binding) {
+        var source = parse(binding.source),
+            destination = parse(binding.destination);
+        dashboard.getComponent(source.alias, function (sourceModel) {
+          dashboard.getComponent(destination.alias, function (destinationModel) {
+            wire([source.property], function (value) {
+              destinationModel.set(destination.property, value);
+            }, sourceModel);
+          });
+        });
+      });
+    }, model);
+
+    function parse(bindingStr){
+      var arr = bindingStr.split('.');
+      return {
+        alias: arr[0],
+        property: arr[1]
+      };
+    }
+
+    return model;
   }
 });
