4da82543d7552c0e100d05cd685999b76d886bdf
curran <curran.kelleher@gmail.com>
2014-03-30 19:56:50 -0400

    Updated clountLines script

--- prototype/countLines.sh
+++ prototype/countLines.sh
@@ -1,12 +1,13 @@
 # A Unix shell script that displays the line count
 # for each source code file recursively.
 #
-# The goal is to help keep every file under 100 lines of code.
+# The goal is to keep every file under 100 lines of code.
 #
-# By Curran Kelleher 3/27/2014 
+# By Curran Kelleher 3/30/2014 
 
 echo "Source code files:"
-find . -mindepth 1 -name "*.*" |grep -v "node_modules\|README\|lib\|docs\|spec" | xargs wc -l
+find . -mindepth 1 -name "*.*" |grep -v "spec\|node_modules\|README\|lib\|docs\|csv\|XLS\|json\|png" | xargs wc -l
 
 echo "Unit tests:"
-find . -mindepth 1 -name "*.*" |grep -v "node_modules\|README\|lib\|docs\|src\|\.png" | xargs wc -l
+cd spec
+find . -mindepth 1 -name "*.*" |grep -v "spec\|node_modules\|README\|lib\|docs\|csv\|XLS\|json\|png" | xargs wc -l
