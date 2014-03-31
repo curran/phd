f1ab3319a27c2bd3283df3402e568ce69be670de
curran <curran.kelleher@gmail.com>
2014-03-31 19:49:17 -0400

    Updated countLines script to exclude comments and blank lines

--- prototype/countLines.sh
+++ prototype/countLines.sh
@@ -3,11 +3,27 @@
 #
 # The goal is to keep every file under 100 lines of code.
 #
-# By Curran Kelleher 3/30/2014 
+# By Curran Kelleher 3/31/2014 
 
-echo "Source code files:"
-find . -mindepth 1 -name "*.*" |grep -v "spec\|node_modules\|README\|lib\|docs\|csv\|XLS\|json\|png" | xargs wc -l
+excluded="spec\|node_modules\|README\|lib\|docs\|csv\|XLS\|json\|png"
 
-echo "Unit tests:"
+countLines(){
+  # $total is the total lines of code counted
+  total=0
+  # -mindepth exclues the current directory (".")
+  for file in `find . -mindepth 1 -name "*.*" |grep -v "$excluded"`; do
+    # First sed: only count lines of code that are not commented with //
+    # Second sed: don't count blank lines
+    # $numLines is the lines of code
+    numLines=`cat $file | sed '/\/\//d' | sed '/^\s*$/d' | wc -l`
+    total=$(($total + $numLines))
+    echo "  " $numLines $file
+  done
+  echo "  " $total in total
+}
+
+echo Source code files:
+countLines
+echo Unit tests:
 cd spec
-find . -mindepth 1 -name "*.*" |grep -v "spec\|node_modules\|README\|lib\|docs\|csv\|XLS\|json\|png" | xargs wc -l
+countLines
