92b073e0b91e8a15e36884a52cd4b6f7c4a49c80
curran <curran.kelleher@gmail.com>
2014-04-01 09:43:38 -0400

    Added listTodos.sh script

---
+++ prototype/listTodos.sh
@@ -0,0 +1,24 @@
+# A Unix shell script that lists TODOs across source files.
+#
+# By Curran Kelleher 4/1/2014 
+
+# $excluded is a regex for paths to exclude from search 
+excluded="spec\|node_modules\|README\|lib\|docs\|csv\|XLS\|json\|png"
+
+countLines(){
+  # -mindepth exclues the current directory (".")
+  for file in `find . -mindepth 1 -name "*.*" |grep -v "$excluded"`; do
+    if [ `grep TODO $file -n | wc -l` != 0 ]
+      then
+        echo $file
+        grep -n TODO $file
+        echo
+    fi
+  done
+}
+
+echo Source code files:
+countLines
+echo Unit tests:
+cd spec
+countLines
