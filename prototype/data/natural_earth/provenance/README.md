39e146b08ed85d6f52d5bf0dfbf798ce31fb1bd2
curran <curran.kelleher@gmail.com>
2014-04-09 15:51:15 -0400

    Added Natural Earth country boundaries data

---
+++ prototype/data/natural_earth/provenance/README.md
@@ -0,0 +1,3 @@
+This directory contains a script that downloads data from [Natural Earth](http://www.naturalearthdata.com/downloads/110m-cultural-vectors/) and transforms it into [TopoJSON](https://github.com/mbostock/topojson) for visualizing geographic regions with D3.
+
+Curran Kelleher 4/9/2014
--- prototype/data/natural_earth/provenance/preprocess.sh
+++ prototype/data/natural_earth/provenance/preprocess.sh
@@ -1,13 +1,28 @@
 # A Bash script for fetching country boundary data fron Natural Earth
 # and transforming them into a format usable by D3 using TopoJSON.
 #
-# Inspired by Mike Bostock's "Let's Make a Map"
+# Draws from Mike Bostock's "Let's Make a Map"
 # http://bost.ocks.org/mike/map/ 
+#
+# See also org2org documentation:
+# http://www.gdal.org/ogr2ogr.html
 #
 # By Curran Kelleher 4/9/2014
 
-# Fetch the countries ZIP file from Natural Earth
+echo Fetching countries ZIP file from Natural Earth
 curl -L -O http://www.naturalearthdata.com/http//www.naturalearthdata.com/download/110m/cultural/ne_110m_admin_0_countries.zip
 
-# Clean up by removing the original ZIP file
-rm ne_110m_admin_0_countries.zip
+echo Unzipping the file
+unzip ne_110m_admin_0_countries.zip
+
+echo Converting the .shp file to GeoJSON
+ogr2ogr -f GeoJSON countriesGeoJSON.json ne_110m_admin_0_countries.shp
+
+echo Converting GeoJSON to TopoJSON
+topojson -p name -o ../countries.json countriesGeoJSON.json
+
+echo Clean up
+rm ne_110m_admin_0_countries*
+rm countriesGeoJSON.json
+
+echo Done! Created ../countries.json
