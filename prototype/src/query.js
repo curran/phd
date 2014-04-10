1ba8f6365dba6c4542737ab38893dc3c8aee7074
curran <curran.kelleher@gmail.com>
2014-04-09 21:09:47 -0400

    Started generalizing data cube query interface

--- prototype/src/query.js
+++ prototype/src/query.js
@@ -3,23 +3,23 @@
 // Curran Kelleher 4/9/2014
 define([], function () {
   return function (dashboard) {
-    return new Backbone.Model().wire('query', function (dataOptions, set) {
+    var model = new Backbone.Model();
+    model.wire(['source', 'dataSet', 'query'], function (source, dataSet, query) {
+
       // Get the `data` component from the dashboard configuration,
       // which is responsible for loading data sources.
       dashboard.getComponent('data', function (dataComponent) {
 
         // Extract the UDC context from the `data` component.
-        var udc = dataComponent.udc,
-            source = dataOptions.source,
-            dataSet = dataOptions.dataSet;
+        var udc = dataComponent.udc;
 
         // Wait for the data set to load.
         udc.waitFor(source, dataSet, function () {
 
           // Query the data cube for the X dimension domain.
           /* TODO refactor the API so (source, dataSet) are not needed */
-          var xDomainCodes = udc.getDomain(source, dataSet, dataOptions.x),
-              xDomainCodeList = udc.getCodeList(source, dataSet, dataOptions.x),
+          var xDomainCodes = udc.getDomain(source, dataSet, query.x),
+              xDomainCodeList = udc.getCodeList(source, dataSet, query.x),
 
               /* TODO generalize handling of X dimensions, special case Time */
               // Parse X dimension domain values into JS Date objects.
@@ -27,14 +27,13 @@ define([], function () {
                 return new Date(yearStr, 0);
               }),
               // Use the `slice` option as the basis for the current cell.
-              cell = _.clone(dataOptions.slices);
+              cell = _.clone(query.slices);
 
-          // Call the callback with (x, y) pairs.
-          set('result', xDomainCodes.map(function (code, i) {
+          model.set('result', xDomainCodes.map(function (code, i) {
 
             // Set the X dimension value for the current cell
             // to the X domain value.
-            cell[dataOptions.x] = {
+            cell[query.x] = {
               code: code,
               codeList: xDomainCodeList
             };
@@ -47,11 +46,12 @@ define([], function () {
 
               // * `y` The measure value from the data cube for
               // the current cell and the configured Y measure.
-              y: udc.getValue(source, dataSet, cell, dataOptions.y)
+              y: udc.getValue(source, dataSet, cell, query.y)
             };
           }));
         });
       });
     });
+    return model;
   };
 });
